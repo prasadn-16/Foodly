85979c525fbdf98f361492b1d029bc204b2e7259
prasadn-16 <nagthanep@gmail.com>
2024-04-10 00:32:56 +0530

    added hook into project to filter cards

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,28 @@
 import RestaurantCards from './RestaurantCard'
 import resList from '../utils/mockData.js'
+import { useState } from 'react'
 
-
-
+console.log(resList)
 const Body = () => {
+  const [listOfRestaurants, setListofRestaurant] = useState(resList)
+
   return (
     <div className="body">
-      <div className="search"></div>
+      <div className="filter">
+        <button className='filter-btn' onClick={() => {
+          const filteredList = listOfRestaurants.filter((res) => {
+            return res.data?.avgRating > 4
+          })
+          setListofRestaurant(filteredList)
+        }
+        }>
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container ">
-      {resList.map((card)=><RestaurantCards key={card.data.id} resCard={card}/>)}
+        {listOfRestaurants.map((card) => <RestaurantCards key={card.data.id} resCard={card} />)}
       </div>
     </div>
   )
 }
-
 export default Body;
